f577a8fb6596a69c6cca868589f2c1624d67ceeb
Tan-maddy <tshresth_bemba19@thapar.edu>
2022-02-26 15:30:40 +0530

    gg

--- assets/js/main.js
+++ assets/js/main.js
@@ -249,7 +249,7 @@
 
   var i = 0;
 var txt = 'AR/VR Developer :)'; /* The text */
-var speed = 100; /* The speed/duration of the effect in milliseconds */
+var speed = 200; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
   if (i < txt.length) {
